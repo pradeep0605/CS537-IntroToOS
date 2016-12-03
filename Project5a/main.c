7abc431ae59c5338df60b0eb58d0f424d17c2bfe
Karan Talreja <karan@cs.wisc.edu>
2016-12-03 13:35:47 -0600

    Project 5a: Working Rule 3 and 4

--- Project5a/main.c
+++ Project5a/main.c
@@ -1,5 +1,3 @@
-#include "include/types.h"
-#include "include/fs.h"
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -9,6 +7,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define stat xv6_stat  // avoid clash with host struct stat
+#define dirent xv6_dirent  // avoid clash with host struct stat
+#include "include/types.h"
+#include "include/fs.h"
+#include "include/stat.h"
+#undef stat
+#undef dirent
 
 char output[1000] = {0};
 #define BLOCK_SIZE (512)
@@ -32,6 +37,40 @@ void usage_and_exit() {
   exit(1);
 }
 
+void* rsect (uint sect, void* img_ptr) {
+  return img_ptr + (sect*BSIZE);
+}
+
+int seen_root = 0;
+void check_dir_inode (uint inode_num, struct dinode* dip, void* img_ptr) {
+  uint file_size = dip->size;
+  uint n_blocks = file_size / 512;
+  int i;
+  char *buf;
+  int seen_self_dir = 0, seen_parent_dir = 0;
+  if (n_blocks < NDIRECT) {
+    for (i=0; i< n_blocks; i++) {
+      buf = rsect(dip->addrs[i], img_ptr);
+      struct xv6_dirent *dir_entry = (struct xv6_dirent*)buf;
+      while (0 != dir_entry->inum) {
+        if (0 == seen_self_dir && (0 == strcmp(dir_entry->name, "."))) seen_self_dir = 1;
+        if (0 == seen_parent_dir && (0 == strcmp(dir_entry->name, ".."))) {
+          seen_parent_dir = 1;
+          if (0 == seen_root && dir_entry->inum == inode_num && inode_num == 1) seen_root = 1;
+        }
+//        printf ("%d,%s,%d\n",inode_num, dir_entry->name, dir_entry->inum);
+        dir_entry++;
+      }
+    }
+  }
+  if (!(seen_self_dir & seen_parent_dir)) {
+    fscheck_perror("directory not properly formatted.\n");
+    exit(1);
+  }
+}
+
+// unused | superblock | inode table | bitmap (data) | data blocks
+
 int
 main(int argc, char* argv[]) {
   if (argc > 2) usage_and_exit();
@@ -50,21 +89,28 @@ main(int argc, char* argv[]) {
   void* img_ptr = mmap(NULL, sbuf.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
   assert(img_ptr != MAP_FAILED);
 
-  struct superblock* sb = (struct superblock*)(img_ptr + BSIZE);
+  struct superblock* sb = (struct superblock*)rsect(1, img_ptr);
 
   int i;
-  struct dinode *dip = (struct dinode*) (img_ptr + (2*BSIZE));
+  struct dinode *dip = (struct dinode*)rsect(2, img_ptr);
   for (i = 0; i< sb->ninodes; i++) {
     switch(dip->type) {
       case 0:
-      case 1:
-      case 2:
-      case 3:
+      case T_FILE:
+      case T_DEV:
         break;
+      case T_DIR: {
+        check_dir_inode (i, dip, img_ptr);
+        break;
+      }
       default:
       fscheck_perror("bad inode.\n");
     }
     dip++;
+  }
+  if (0 == seen_root) {
+    fscheck_perror("ERROR: root directory does not exist.\n");
+    exit(1);
   }
   return 0;
 }
